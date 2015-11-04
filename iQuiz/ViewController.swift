e0bbf8b37948723cf533bbe5f0c87daf334070c2
Smyth May <william.may@motorolasolutions.com>
2015-11-03 19:51:45 -0800

    Finished part 1 of assignment

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIToolbarDelegate {
     
     let subjects : [String] = ["Mathematics", "Marvel Super Heroes", "Science"];
+    let descriptions : [String] = ["One of the oldest fields of study", "Pop culture and super heroes!", "The study of life, the universe, and everything"];
     
     @IBOutlet
     var tableView : UITableView!;
+    @IBOutlet weak var settingsButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell");
+        settingsButton.action = "settingsAlert:";
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,14 +34,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell : UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")!;
+        var cell : UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!;
+        cell = UITableViewCell(style: UITableViewCellStyle.Subtitle,
+            reuseIdentifier: "cell");
         cell.textLabel?.text = self.subjects[indexPath.row];
         cell.imageView!.image = UIImage(named : self.subjects[indexPath.row]);
+        cell.detailTextLabel?.text = self.descriptions[indexPath.row];
         return cell;
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+        NSLog("Pressed");
+    }
+    
+    func settingsAlert(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Settings", message: "Settings Go Here", preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            
+        }
+        alertController.addAction(OKAction);
+        self.presentViewController(alertController, animated: true) {
+            
+        }
     }
 
 
